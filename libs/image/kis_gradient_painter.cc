cd524243d725c49f560169b247d04c5fa5313d2e
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-11-14 19:23:19 -0500

    spelling: smoothing
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- libs/image/kis_gradient_painter.cc
+++ libs/image/kis_gradient_painter.cc
@@ -1117,7 +1117,7 @@ bool KisGradientPainter::paintGradient(const QPointF& gradientVectorStart,
     // The following combinations of options have aliasing artifacts
     // where the first color meets the last color of the gradient.
     // so antialias threshold is used to compute if the pixel is in
-    // the smothing area. Then linear interpolation is used to blend
+    // the smoothing area. Then linear interpolation is used to blend
     // between the first and last colors
     if (antiAliasThreshold > DBL_EPSILON) {
         if ((m_d->shape == GradientShapeLinear || m_d->shape == GradientShapeBiLinear ||
